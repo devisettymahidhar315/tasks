7586b46db752453402bd6bb8170fa64d39c92aea
mahi <mahidhardevisetty@gmail.com>
2024-04-30 17:29:43 +0530

    create the code for multi service using flask and converted into docker'

--- microservices deploying in kubernetes/app2.py
+++ microservices deploying in kubernetes/app2.py
@@ -1,19 +1,31 @@
-from flask import Flask, jsonify
-import requests
 
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
 
-@app.route('/orders/<int:user_id>')
-def get_orders(user_id):
-    user_info_response = requests.get(f'http://user-service:5001/users/{user_id}')
-    if user_info_response.status_code == 200:
-        user_info = user_info_response.json()
-        # For demonstration, assuming orders are hardcoded
-        orders = {"orders": ["Order 1", "Order 2"]}
-        orders.update(user_info)
-        return jsonify(orders)
-    else:
-        return jsonify({"error": "User not found"}), 404
+from sqlalchemy.sql import func
 
-if __name__ == '__main__':
-    app.run(port=5002)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app2 = Flask(__name__)
+app2.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app2.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app2)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    lastname = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(80), unique=True, nullable=False)
+    age = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    bio = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<Student {self.firstname}>'
+
+if __name__ == "__main__":
+    app2.run(debug=True)
---
+++ microservices deploying in kubernetes/flask/app.py
@@ -0,0 +1,47 @@
+import os
+# from flask import Flask, redirect, url_for, render_template
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+# db1=SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    
+
+class Staff(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+
+
+@app.route('/student')
+def student():
+    return render_template('student.html', students=Student)
+
+@app.route('/staff')
+def staff():
+    return render_template('staff.html', staf=Staff)
+
+@app.route('/user/<name>')
+def user(name):
+    if name == 'student':
+        return redirect(url_for('student'))
+    elif name == 'staff':
+        return redirect(url_for('staff'))
+    else:
+        return f'Hello {name}!'
+
+if __name__ == '__main__':
+    app.run()
