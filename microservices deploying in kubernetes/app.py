d796267b0550d0d294775141845e5c4a80d7a929
mahi <mahidhardevisetty@gmail.com>
2024-05-03 16:47:37 +0530

    updated multi microservices

--- microservices deploying in kubernetes/app.py
+++ microservices deploying in kubernetes/app.py
@@ -1,14 +1,40 @@
-from flask import Flask, redirect, url_for
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+from sqlalchemy.sql import func
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+    
+
+class Staff(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100), nullable=False)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the student and staff details!'
+
 
 @app.route('/student')
 def student():
-    return 'This is student.'
+    return render_template('student.html', students=Student)
 
 @app.route('/staff')
 def staff():
-    return 'This is staff.'
+    return render_template('staff.html', staf=Staff)
 
 @app.route('/user/<name>')
 def user(name):
@@ -20,4 +46,8 @@ def user(name):
         return f'Hello {name}!'
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
+
+
+
+
